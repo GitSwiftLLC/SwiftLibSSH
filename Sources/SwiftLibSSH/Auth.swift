a16bfa431533f01151189cf9a1665ad4c3de5721
GitSwiftDev <developer@gitswift.com>
2025-03-05 21:31:46 +0800

    Enhance pointer handling and authentication methods in SwiftLibSSH

--- Sources/SwiftLibSSH/Auth.swift
+++ Sources/SwiftLibSSH/Auth.swift
@@ -135,10 +135,12 @@ public extension SwiftLibSSH {
             return await call {
                 let code = self.callSSH2 {
                     libssh2_userauth_keyboard_interactive_ex(rawSession, self.user, self.user.countUInt32) { _, _, _, _, numPrompts, prompts, responses, abstract in
-                        guard let abstract, let from = abstract.pointee else {
+                        guard let abstract = abstract else {
                             return
                         }
-                        let ssh = SwiftLibSSH.getSSH(from: from)
+                        guard let ssh: SwiftLibSSH = abstract.load() else {
+                            return
+                        }
                         for i in 0 ..< Int(numPrompts) {
                             guard let promptI = prompts?[i], let text = promptI.text else {
                                 continue
@@ -172,5 +174,35 @@ public extension SwiftLibSSH {
             return false
         }
         return libssh2_userauth_authenticated(rawSession) == 1
+    }
+}
+
+/// An extension to `UnsafeRawPointer` that provides additional functionality.
+///
+/// This extension includes methods for loading data of a specified type and accessing SSH-related data.
+public extension UnsafeRawPointer {
+    /// Loads data of the specified type from the raw pointer.
+    ///
+    /// - Returns: The data of type `T` loaded from the raw pointer.
+    func load<T>() -> T {
+        load(as: T.self)
+    }
+}
+
+public extension UnsafeMutablePointer {
+    /// A computed property that returns an `UnsafeRawPointer` pointing to the memory address of the current instance.
+    ///
+    /// - Returns: An `UnsafeRawPointer` pointing to the memory address of the current instance.
+    var address: UnsafeRawPointer {
+        UnsafeRawPointer(self)
+    }
+
+    /// Loads a value of type `T` from the memory location pointed to by the address.
+    ///
+    /// - Returns: The value of type `T` loaded from the memory.
+    ///
+    /// - Note: This function assumes that the memory is properly aligned and initialized for type `T`.
+    func load<T>() -> T {
+        address.load()
     }
 }
